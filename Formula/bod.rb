6dab7ac987718377f9a88e0081f3bc8af6a21ab6
isaaguilar <isa@aguilarstory.com>
2026-03-16 22:53:05 -0400

    Update bod to v0.3.0

--- Formula/bod.rb
+++ Formula/bod.rb
@@ -4,20 +4,20 @@ class Bod < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/GalleyBytes/board-of-directors/releases/download/v0.2.0/bod-v0.2.0-aarch64-apple-darwin.tar.gz"
-      sha256 "f84c101cd8cadd15c5a8dbea72a3ffd8ea2b5b21ca7da08471362579007ff683"
+      url "https://github.com/GalleyBytes/board-of-directors/releases/download/v0.3.0/bod-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "0526860913cf44f2baeae0ebae4195e9bc54b711a1e854a4e76b7bec26a653f4"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/GalleyBytes/board-of-directors/releases/download/v0.2.0/bod-v0.2.0-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "fb00375edf25f09f8c4ab5207a6a3ef07a3a656696e393ef9b8a28ca162bc956"
+      url "https://github.com/GalleyBytes/board-of-directors/releases/download/v0.3.0/bod-v0.3.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "8837fdbe74151daa86cff0a928e8edd31253ffb6f08413588c67b11cec8dc043"
     end
 
     if Hardware::CPU.intel?
-      url "https://github.com/GalleyBytes/board-of-directors/releases/download/v0.2.0/bod-v0.2.0-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "16d8bb5ddeabdd9927aa34493e418a11aa8cad3058ca5e6a0167dc9bcfd43c97"
+      url "https://github.com/GalleyBytes/board-of-directors/releases/download/v0.3.0/bod-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "4d4d167f313410924df3446892846fb9bc6b89061dd0101f43d4b8fb8aaa2e59"
     end
   end
 
